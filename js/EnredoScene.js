68c54e5b24200549e3ff72d9f8bb1cece2918b53
Gustavo <gustavogomes1108@gmail.com>
2025-05-31 16:59:24 -0300

    .

--- js/EnredoScene.js
+++ js/EnredoScene.js
@@ -96,7 +96,7 @@ class EnredoScene extends Phaser.Scene {
 
         startButton.on('pointerup', () => {
             this.enredoMusic.stop();
-            this.scene.start('GameScene');
+            this.scene.start('Fase1Scene');
         });
     }
 }
